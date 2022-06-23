e74e52e3c4549d4995e2feded5fcc529ac73b033
YoGa-jpg <antondavidovich45@gmail.com>
2022-06-23 19:55:49 +0300

    Add steam interraction class

--- CommanderBot/Model/Steam.cs
+++ CommanderBot/Model/Steam.cs
@@ -10,18 +10,11 @@ namespace CommanderBot.Model
 {
     class Steam
     {
-        private OpenDota Dota;
-        public Steam()
+        public static ulong GetSteam32(string url)
         {
-            Dota = new OpenDota();
-        }
-
-        public async Task<int?> GetProfile(string url)
-        {
-            var id = new Regex("https://steamcommunity.com/profiles/(.*)/").Match(url)
+            var steam64 = new Regex("https://steamcommunity.com/profiles/(.*)/").Match(url)
                 .Groups[1].Value;
-            var player = await Dota.Players.GetPlayerAsync(long.Parse(id) - 76561197960265728);
-            return player.MmrEstimate.Estimate.Value;
+            return ulong.Parse(steam64) - 76561197960265728;
         }
     }
 }
