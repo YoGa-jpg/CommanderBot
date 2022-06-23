ad803afe39c10fc812537a6223603385454f581d
YoGa-jpg <antondavidovich45@gmail.com>
2022-06-23 19:54:07 +0300

    Add profile context (only with Steam)

---
+++ CommanderBot/Model/Profile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommanderBot.Model
+{
+    class Profile
+    {
+        public ulong DiscordId { get; set; }
+        public ulong DotaId { get; set; }
+    }
+}
---
+++ CommanderBot/Model/ProfileContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CommanderBot.Model
+{
+    class ProfileContext : DbContext
+    {
+        public DbSet<Profile> Profiles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=GameProfileDb;Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Profile>()
+                .HasKey(profile => profile.DiscordId);
+            modelBuilder.Entity<Profile>()
+                .Property(pr => pr.DiscordId)
+                .ValueGeneratedNever()
+                .IsRequired();
+            //modelBuilder.
+            //    Entity<Track>()
+            //    .HasOne(track => track.Guild)
+            //    .WithMany(guild => guild.Tracks)
+            //    .HasForeignKey(track => track.GuildId);
+
+            //modelBuilder
+            //    .Entity<Guild>()
+            //    .HasOne(guild => guild.TextChannel)
+            //    .WithOne(chan => chan.Guild)
+            //    .HasForeignKey<TextChannel>(chan => chan.GuildId);
+
+            //modelBuilder.Entity<TextChannel>()
+            //    .Property(chan => chan.Id)
+            //    .ValueGeneratedNever();
+        }
+    }
+}
