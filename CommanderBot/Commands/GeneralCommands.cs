406e8444c6fe58fd417c27c120b7204813177a45
YoGa-jpg <antondavidovich45@gmail.com>
2022-06-23 19:53:34 +0300

    Add link commands

--- CommanderBot/Commands/GeneralCommands.cs
+++ CommanderBot/Commands/GeneralCommands.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using CommanderBot.Model;
 using DSharpPlus;
@@ -13,9 +16,30 @@ namespace CommanderBot.Commands
         [Command("link")]
         public async Task LinkAccount(CommandContext ctx, string url)
         {
-            Steam steam = new Steam();
-            var profile = await steam.GetProfile(url);
-            await ctx.RespondAsync(profile.ToString());
+            using (ProfileContext db = new ProfileContext())
+            {
+                switch (url)
+                {
+                    case var x when x.Contains("steam"):
+                        try
+                        {
+                            db.Profiles.Single(profile => profile.DiscordId == ctx.User.Id).DotaId =
+                                Steam.GetSteam32(url);
+                        }
+                        catch (Exception e)
+                        {
+                            db.Profiles.Add(new Profile()
+                            {
+                                DiscordId = ctx.User.Id,
+                                DotaId = Steam.GetSteam32(url)
+                            });
+                        }
+                        await db.SaveChangesAsync();
+
+                        await ctx.RespondAsync("Профиль подключен!");
+                        break;
+                }
+            }
         }
     }
 }
