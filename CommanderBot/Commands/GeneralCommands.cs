28b8d66288dfb3a2263a38a4f1265b46f17c651d
YoGa-jpg <antondavidovich45@gmail.com>
2022-06-24 19:20:50 +0300

    Add permitted set and user profile

--- CommanderBot/Commands/GeneralCommands.cs
+++ CommanderBot/Commands/GeneralCommands.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Threading.Tasks;
+using DSharpPlus;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 using ProfileBot.Model;
@@ -27,6 +28,35 @@ namespace ProfileBot.Commands
                             db.Profiles.Add(new Profile()
                             {
                                 DiscordId = ctx.User.Id,
+                                DotaId = Steam.GetSteam32(url)
+                            });
+                        }
+                        await db.SaveChangesAsync();
+
+                        await ctx.RespondAsync("Профиль подключен!");
+                        break;
+                }
+            }
+        }
+
+        [Command("set"), RequirePermissions(Permissions.Administrator)]
+        public async Task SetAccount(CommandContext ctx, string url, string mention)
+        {
+            using (ProfileContext db = new ProfileContext())
+            {
+                switch (url)
+                {
+                    case var x when x.Contains("steam"):
+                        try
+                        {
+                            db.Profiles.Single(profile => profile.DiscordId == ctx.Message.MentionedUsers.First().Id).DotaId =
+                                Steam.GetSteam32(url);
+                        }
+                        catch (Exception e)
+                        {
+                            db.Profiles.Add(new Profile()
+                            {
+                                DiscordId = ctx.Message.MentionedUsers.First().Id,
                                 DotaId = Steam.GetSteam32(url)
                             });
                         }
